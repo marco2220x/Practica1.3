8ab9d3da83dd800554d6504717dde6febd319ff8
Daniel Vega <vegad8510@gmail.com>
2023-09-24 23:38:25 -0600

    Final Update

--- HoughTform/TransformadaHough.py
+++ HoughTform/TransformadaHough.py
@@ -1,14 +1,17 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-from Bordes import Canny
-from Bordes import Canny2
+from bordes import Canny
+from bordes import Canny2
 
-image_file = 'um_000014.png'
+image_file = 'Images/um_000001.png'
 original_image = cv2.imread(image_file, 1)
 grayscale_image = cv2.imread(image_file, 0)
 
-canny_edges = Canny.deteccion_bordes_canny(grayscale_image)
+blurred = cv2.GaussianBlur(grayscale_image, (5, 5), 1.5)
+canny_edges = cv2.Canny(blurred, 100, 200)
+#canny_edges = Canny.deteccion_bordes_canny(grayscale_image)
+#canny_edges = Canny.deteccion_bordes_canny(grayscale_image)
 
 # Acumulador de hough para la imagen
 def hough_lines_acc(img, rho_resolution=1, theta_resolution=1):
@@ -19,7 +22,7 @@ def hough_lines_acc(img, rho_resolution=1, theta_resolution=1):
 
     # Crear el acumulador Hough con dimensiones iguales de rhos y thetas
     H = np.zeros((len(rhos), len(thetas)), dtype=np.uint64)
-    y_idxs, x_idxs = np.nonzero(img) # find all edge (nonzero) pixel indexes
+    y_idxs, x_idxs = np.nonzero(img) # encontrar todos los índices de píxeles de borde (distintos de cero)
 
     for i in range(len(x_idxs)): #Recorremos los puntos del borde
         x = x_idxs[i]
@@ -32,4 +35,82 @@ def hough_lines_acc(img, rho_resolution=1, theta_resolution=1):
 
     return H, rhos, thetas
 
+# Espacio de Hough
+def hough_peaks(H, num_peaks, nhood_size=3):
+
+    #Recorrer el numero de picos 
+    indicies = []
+    H1 = np.copy(H) #Crear una copia de H
+    for i in range(num_peaks):
+        idx = np.argmax(H1) # Encontrar lo maximos de de la matriz Hough
+        H1_idx = np.unravel_index(idx, H1.shape) # Reasignar 
+        indicies.append(H1_idx)
+
+        idx_y, idx_x = H1_idx # Separamos los indices 
+        # Elegir los valores apropiados si idx_x se encuentra cerca de los bordes
+        if (idx_x - (nhood_size/2)) < 0: min_x = 0
+        else: min_x = idx_x - (nhood_size/2)
+        if ((idx_x + (nhood_size/2) + 1) > H.shape[1]): max_x = H.shape[1]
+        else: max_x = idx_x + (nhood_size/2) + 1
+
+        # Eligir los valores apropiados, si idx_y está demasiado cerca de los bordes
+        if (idx_y - (nhood_size/2)) < 0: min_y = 0
+        else: min_y = idx_y - (nhood_size/2)
+        if ((idx_y + (nhood_size/2) + 1) > H.shape[0]): max_y = H.shape[0]
+        else: max_y = idx_y + (nhood_size/2) + 1
+
+        # Vincular cada índice (por el tamaño de la vecindad y establecer todos los valores en 0
+        for x in range(int(min_x), int(max_x)):
+            for y in range(int(min_y), int(max_y)):
+                # eliminar vecindarios en H1
+                H1[y, x] = 0
+
+                # Resaltar picos en H
+                if (x == min_x or x == (max_x - 1)):
+                    H[y, x] = 255
+                if (y == min_y or y == (max_y - 1)):
+                    H[y, x] = 255
+
+    # Devolver los índices y el espacio de Hough original con los puntos seleccionados
+    return indicies, H
+
+
+#Graficar el espacio de Hough
+def plot_hough_acc(H):
+    fig = plt.figure(figsize=(10, 10))
+    
+    plt.imshow(H, cmap='jet')
+    plt.xlabel('Direccion Theta'), plt.ylabel('Direccion Rho')
+    plt.tight_layout()
+    plt.show()
+
+
+#Graficar las lineas de Hough
+def hough_lines_draw(img, indicies, rhos, thetas):
+    ''' Toma indices de una tabla rhos y thetas y dibuja y dibuja las lineas 
+    en la imagen que corresponde a estos valores'''
+    for i in range(len(indicies)):
+        rho = rhos[indicies[i][0]]
+        theta = thetas[indicies[i][1]]
+        a = np.cos(theta)
+        b = np.sin(theta)
+        x0 = a*rho
+        y0 = b*rho
+        x1 = int(x0 + 1000*(-b))
+        y1 = int(y0 + 1000*(a))
+        x2 = int(x0 - 1000*(-b))
+        y2 = int(y0 - 1000*(a))
+
+        cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+
 H, rhos, thetas = hough_lines_acc(canny_edges) # Calcula la acumuladora de Hough
+indicies, H = hough_peaks(H, 5, nhood_size=11) # Encuentra los picos en la acumuladora de Hough
+plot_hough_acc(H) # Mostrar el espacio de Hough
+'''Toma la imagen original, los índices de los picos, las tablas de valores rho y theta, 
+Dibuja las líneas correspondientes en la imagen original.'''
+hough_lines_draw(original_image, indicies, rhos, thetas)
+# Muestra la imagen con las lineas de transformacion de Hough
+cv2.imshow('Deteccion de lineas con la transformada de Hough', original_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-Práctica 1 Integrantes: Evelyn Solano Portillo (Eeveeelyyyn) Aranza Michelle Gutierrez Jiménez (AranzaMich) Daniel Vega Rodríguez (DanVer2002) Marco Antonio Castelan Rosete (marco2220x)
+# Transformada de Hough y Etiquetado de componentes conectados 🗠
+Práctica 1.3  - Vision Artificial
+> La Transformada de Hough y el etiquetado de componentes conectados son dos técnicas fundamentales en el campo de procesamiento de imágenes y visión por computadora que se utilizan para detectar patrones y analizar imágenes en busca de regiones o características específicas
+electrónico extrae automáticamente información útil a partir de una
+imagen digital para una aplicación específica.
+
+## En esta práctica se realiza lo siguiente:
++ Transfromada de Hough
+    + Realizar la detección de líneas sobre un subconjunto aleatorio de 50 imágenes del conjunto de datos kitti
+    + Ocupar el algoritmo de Canny desarrollado para la deteción de bordes
+    + Mostrar la salida de la imagen donde se marcan las líneas detectadas y una imagen del espacio de Hough.
+
++ Etiquetado de componentes etiquetados
+    + Identificar los componentes presentes en una muestra de 50 imágenes del conjunto de datos de Artificial Mercosur License Plates
+    + La entrada del algoritmo es una imagen binaria, se definine el umbral para binarizar las imágenes u ocupar alguna técnica automática que venga en opencv
+    + OcupaR la estructura de datos union-find e imprimir al final de la primera corrida todos los componentes detectados, quienes son padres y quienes son hijos
+    + En la imagen de salida colorear los componentes que se identifiquen de distintos colores
+
+## Autores
+| [<img src="https://avatars.githubusercontent.com/u/113084234?v=4" width=115><br><sub>Aranza Michelle Gutierrez Jimenez</sub>](https://github.com/AranzaMich) |  [<img src="https://avatars.githubusercontent.com/u/113297618?v=4" width=115><br><sub>Evelyn Solano Portillo</sub>](https://github.com/Eveeelyyyn) |  [<img src="https://avatars.githubusercontent.com/u/112792541?v=4" width=115><br><sub>Marco Antonio Castelan Rosete</sub>](https://github.com/marco2220x) | [<img src="https://avatars.githubusercontent.com/u/113079687?v=4" width=115><br><sub>Daniel Vega Rodríguez</sub>](https://github.com/DanVer2002) |
+| :---: | :---: | :---: | :---: |
+
